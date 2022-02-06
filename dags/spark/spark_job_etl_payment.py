5bfcef7fba69467343dd413af0683daf63d51d61
Jozimar Back <jozimar5@gmail.com>
2022-02-06 12:50:31 -0300

    Implementation of connection to postgres with host.docker.internal

--- dags/spark/spark_job_etl_payment.py
+++ dags/spark/spark_job_etl_payment.py
@@ -1,22 +1,18 @@
 from pyspark.sql import SparkSession, functions as f
 from pyspark import SparkConf, SparkContext
 
-sc = SparkContext(conf=SparkConf()).getOrCreate()
-# spark = SparkSession(sc).builder.config("spark.jars.packages","org.postgresql:postgresql:42.2.10").getOrCreate()
-spark = SparkSession.builder.config("spark.jars.packages","org.checkerframework:checker-qual:3.5.0;com.github.waffle:waffle-jna:1.9.1;org.postgresql:postgresql:jar:42.2.4.jre7")\
-    .config("spark.jars","postgresql-42.2.4.jar").getOrCreate()
+sc = SparkContext(conf=SparkConf().set("spark.jars.packages","org.postgresql:postgresql:42.2.10")).getOrCreate()
+spark = SparkSession(sc)
 
-df = spark.read.json("./dags/data/payments/0a13a9b3b19ecc9a9e48e2d30bb207ca.json")\
-    .withColumn("paymentDate", f.to_timestamp("paymentDate"))\
-    .withColumn("paymentValue", f.col("paymentValue").astype("double"))
-# print(df.printSchema())
-# print("done")
-df.write.format("jdbc")\
-    .option("url","jdbc:postgresql://172.17.0.1:5432/de_challenge")\
-    .option("dbtable","public.payments")\
-    .option("user", "postgres")\
-    .option("password", "changeme")\
-    .option("driver", "org.postgresql.Driver")\
-    .mode("overwrite")\
-    .save()
+df = (spark.read.json("../data/payments/*.json")
+    .withColumn("paymentDate", f.to_timestamp("paymentDate"))
+    .withColumn("paymentValue", f.col("paymentValue").astype("double")))
 
+df.write.jdbc(url="jdbc:postgresql://host.docker.internal:5432/db_challenge"
+        ,table="payments"
+        ,properties={
+            "user": "postgres",
+            "password": "changeme",
+            "driver": "org.postgresql.Driver"
+    }
+    ,mode="overwrite")
